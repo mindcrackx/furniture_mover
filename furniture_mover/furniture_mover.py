7dede506a5867f32d332e54ad10c2bf38218097b
mindcrackx <m.plaumann@protonmail.com>
2020-07-06 23:40:33 +0200

    add filter feature
    Regex filter for document ID.
    Add test for new feature.

--- furniture_mover/furniture_mover.py
+++ furniture_mover/furniture_mover.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import re
 import sys
 from pathlib import Path
 from typing import Union
@@ -51,7 +52,6 @@ class FurnitureMover:
         try:
             async with aiofiles.open(infile, mode="r", encoding="utf-8") as inf:
                 data = json.loads(await inf.read())
-                print(data)
         except Exception as e:
             sys.exit(f"Exception opening or reading file {infile}: {str(e)}")
 
@@ -59,3 +59,24 @@ class FurnitureMover:
             async with aiofiles.open(outfile, mode="w", encoding="utf-8") as outf:
                 for row in data["rows"]:
                     await outf.write(json.dumps(row["doc"], ensure_ascii=False) + "\n")
+
+    @staticmethod
+    async def filter_infile(filter_file: Path, infile: Path) -> None:
+        filters = None
+        try:
+            async with aiofiles.open(filter_file, "r", encoding="utf-8") as inf:
+                filters = json.loads(await inf.read())
+        except Exception as e:
+            sys.exit(f"Exception opening or reading file {filter_file}: {str(e)}")
+
+        for filter_ in filters:
+            async with aiofiles.open(
+                filter_["filepath"], "w", encoding="utf-8"
+            ) as outf:
+                async with aiofiles.open(infile, "r", encoding="utf-8") as inf:
+                    async for line in inf:
+                        data = json.loads(line)
+                        for regex in filter_["regex_filters"]:
+                            if re.match(regex, data["_id"]):
+                                await outf.write(json.dumps(data) + "\n")
+                                break
